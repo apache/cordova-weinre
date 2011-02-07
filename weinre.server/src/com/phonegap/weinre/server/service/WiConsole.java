848ad64d13a78c92041593e886ce816b830f6341
Patrick Mueller <pmuellr@yahoo.com>
2011-02-07 15:04:28 -0500

    connection now happening, but dies shorly afterwards

---
+++ weinre.server/src/com/phonegap/weinre/server/service/WiConsole.java
@@ -0,0 +1,16 @@
+/*
+ * weinre is available under *either* the terms of the modified BSD license *or* the
+ * MIT License (2008). See http://opensource.org/licenses/alphabetical for full text.
+ * 
+ * Copyright (c) 2011 IBM Corporation
+ */
+
+package com.phonegap.weinre.server.service;
+
+public class WiConsole extends InspectorBackend {
+
+    public WiConsole() {
+        super();
+    }
+    
+}
