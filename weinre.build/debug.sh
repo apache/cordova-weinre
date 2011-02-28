04f0a039fab930034e888ba87ca201cdef083373
Patrick Mueller <pmuellr@yahoo.com>
2011-02-28 13:51:38 -0500

    Fixes autoconnect issue. Closes #29
    - main problem was that clientId was not set when the autoconnect
    logic kicked in, in the situation described in the issue
    
    - changed to use connectorId, which is set at this point
    
    - connectorId was numeric at some points, needed to make a string
    
    - drive by fix for debug.sh to change on the cp dirs to point to the
    live output directory

--- weinre.build/debug.sh
+++ weinre.build/debug.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 CWD=`dirname $0`
 WEINRE=`dirname $CWD`
-CP=$WEINRE/weinre.server/bin:$WEINRE/weinre.build/bin:$WEINRE/weinre.build/vendor/cli/commons-cli.jar:$WEINRE/weinre.build/vendor/jetty/jetty.jar:$WEINRE/weinre.build/vendor/json4j/json4j.jar:$WEINRE/weinre.build/vendor/jetty/servlet-api.jar
+CP=$WEINRE/weinre.server/bin:$WEINRE/weinre.build/out:$WEINRE/weinre.build/vendor/cli/commons-cli.jar:$WEINRE/weinre.build/vendor/jetty/jetty.jar:$WEINRE/weinre.build/vendor/json4j/json4j.jar:$WEINRE/weinre.build/vendor/jetty/servlet-api.jar
 PORT=8096
 DEBUG=jdwp=transport=dt_socket,server=y,suspend=n,address=localhost:$PORT
 
